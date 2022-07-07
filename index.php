799781172ad6965236a6e9e27cbe23a7c7f65235
EngineerNaveen <108856644+EngineerNaveen@users.noreply.github.com>
2022-07-07 11:53:40 +0530

    uploading all file
    this is first commit

---
+++ index.php
@@ -0,0 +1,61 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="style/indexStyle.css">
+    <title>Home | Attendance</title>
+</head>
+<body>
+    <!-- conatiner for fullpage -->
+    <div class="container">
+        <!-- container for header only -->
+        <div class="header-container">
+            <div class="header">
+                <!-- logo part -->
+                <div class="logo-container">
+                    <img src="img/logo.png" alt="">
+                </div>
+                <!-- menubar part -->
+                <div class="menubar-container">
+                    <div class="menubar">
+                        <div class="menu">
+                            <a href="#">Home</a>
+                        </div>
+                        <div class="menu">
+                            <a href="#">About</a>
+                        </div>
+                        <div class="menu">
+                            <a href="#">Contact us</a>
+                        </div>
+                    </div>
+                </div>
+                <!-- login logout part -->
+                <div class="login-logout-container">
+                    <div class="login-container">
+                        <button>Login</button>
+                    </div>
+                    <div class="logout-container">
+                        <button>Logout</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- code for ajax call changable content  -->
+        <div class="content-container">
+
+        </div>
+
+        <!-- code for footer part of the website  -->
+        <div class="footer-container">
+
+        </div>
+    </div>
+</body>
+</html>
